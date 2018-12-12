11c6b8b74dbe92fc976874607e0996f4b5b2cada
MiaChi <purple5world@gmail.com>
2018-12-12 16:05:52 +0800

    commit before pull

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
   <div>
-    <h2>故 <span style="color:#DBDDE0">•</span> 事</h2>
+    <h2>故 <img src="/static/favicon.png" class="icon"/> 事</h2>
+    <h6><span style="color:grey;font-size:12px;">发生的都是故事</span></h6>
     <hr/>
-    <h6> <span style="color:grey">发生的都是故事 </span></h6>
   </div>
     <router-view/>
     <hr/>
@@ -24,6 +24,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+}
+.icon {
+  width: 16px;
+  height: 16px;
 }
 </style>
