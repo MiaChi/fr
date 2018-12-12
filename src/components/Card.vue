821ea92846cb9317b4e8be9c584423cd57519593
MiaChi <purple5world@gmail.com>
2018-12-12 16:00:18 +0800

    update reading card and related sub page

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,7 +4,7 @@
       <b-card no-body
         style="max-width: 20rem;"
         >
-      <h4 slot="header">旅行</h4>
+      <h4 slot="header">学习</h4>
         <b-card-body>
             <p class="card-text">
                 Summary of travel
@@ -58,13 +58,14 @@
       <h4 slot="header">读书</h4>
         <b-card-body>
             <p class="card-text">
-                2:Some quick example text to build on the card title and make up the bulk of the card's content.
+              很有意思
             </p>
         </b-card-body>
         <b-list-group flush>
-            <b-list-group-item>Cras justo odio</b-list-group-item>
-            <b-list-group-item>Dapibus ac facilisis in</b-list-group-item>
-            <b-list-group-item>Vestibulum at eros</b-list-group-item>
+            <b-list-group-item><a href="#/track/">冷场 ｜ 李诞</a></b-list-group-item>
+            <b-list-group-item><a href="#/track/">REMOTE | Jason Fried and David Heinemeier Hansson</a></b-list-group-item>
+            <b-list-group-item><a href="#/track/">REWORK | Jason Fried and David Heinemeier Hansson</a></b-list-group-item>
+            <b-list-group-item><a href="#/track/">一只特立独行的猪 ｜ 王小波</a></b-list-group-item>
         </b-list-group>
         <b-card-body>
             <a href="#"
@@ -72,7 +73,7 @@
             <a href="#"
                class="card-link">Another link</a>
         </b-card-body>
-        <b-card-footer>This is a footer</b-card-footer>
+        <!-- <b-card-footer>This is a footer</b-card-footer> -->
         <b-card-img src="https://placekitten.com/480/201"
                     alt="Image"
                     bottom></b-card-img>
@@ -118,7 +119,7 @@ export default {
     //       listItems: [{text: 'item0', linkTo: '#/note'}, {text: 'item1', linkTo: '#/note'}, {text: 'item2', linkTo: '#/note'}],
     //       link: '#/track',
     //       footer: 'This is footer',
-    //       footImg: 'https://placekitten.com/480/210'
+    //       footImg: 'https://placekitten.com/480/204'
     //     },
     //     {
     //       headImg: 'https://placekitten.com/380/199',
@@ -135,12 +136,13 @@ export default {
   created () {
     // https://www.weibo.com/p/1005051263905037/wenzhang
     // https://www.weibo.com/ttarticle/p/show?id=2309404194775615067422
-    // fetchJsonp('https://m.weibo.cn/api/container/getIndex?containerid=2303190002_445_1263905037_WEIBO_ARTICLE_LIST_DETAIL').then(function (response) {
+    // fetchJsonp('https://api.weibo.com/2/favorites.json?access_token=2.00H8NX4BoGuhsC867e46a17a0fmskj').then(function (response) {
     //   console.log('response')
     // }).catch(function (ex) {
     //   console.log('parsing failed', ex)
     // })
     fetch('http://localhost:3000').then(function (sres) {
+      console.log(sres)
       return sres.json()
       // here need use arrow function to make 'this' work as expected
     }).then((sjson) => {
@@ -151,7 +153,7 @@ export default {
       })
       this.cards.push({
         headImg: '',
-        header: 'Travels',
+        header: '旅行',
         subHeader: 'It is a beautiful world',
         listItems: listItems,
         link: 'https://weibo.com/p/1005051263905037/wenzhang',
--- src/components/Track.vue
+++ src/components/Track.vue
@@ -2,12 +2,13 @@
     <Timeline class="narrow-timeline">
         <TimelineItem color="green">
             <Icon type="ios-trophy" slot="dot"></Icon>
-            <span>发布里程碑版本</span>
+            <!-- <span>发布里程碑版本</span> -->
         </TimelineItem>
          <TimelineItem v-for="item in items" :key="item.id">
             <p class="time">{{item.date}}</p>
             <p class="content">{{item.content}}</p>
-            <p><img src="https://pek3a.qingstor.com/warehouse1/c1.jpg" /></p>
+            <p><img :src="item.img"  /></p>
+            <!-- <p><img src="https://pek3a.qingstor.com/warehouse1/c1.jpg" /></p> -->
         </TimelineItem>
     </Timeline>
 </template>
@@ -17,20 +18,24 @@ export default {
     return {
       items: [
         {
-          date: '2018.9.2',
-          cotent: 'Hello, SriLanka'
+          date: '2018.11',
+          content: '冷场 ｜ 李诞',
+          img: '/../static/lengchang.png'
         },
         {
-          date: '2018.01',
-          content: 'Hello, HongKong'
+          date: '2018.8',
+          content: 'REWORK | Jason Fried and David Heinemeier Hansson',
+          img: 'https://pek3a.qingstor.com/warehouse1/c1.jpg'
         },
         {
-          date: '2016.1',
-          content: 'Hello, Dali'
+          date: '2018.6',
+          content: 'REMOTE | Jason Fried and David Heinemeier Hansson',
+          img: 'https://pek3a.qingstor.com/warehouse1/c1.jpg'
         },
         {
-          date: '2015.1',
-          content: 'Hello, LiJiang'
+          date: '2018.6',
+          content: '一只特立独行的猪｜王小波',
+          img: 'https://pek3a.qingstor.com/warehouse1/c1.jpg'
         }
       ]
     }
@@ -49,5 +54,9 @@ export default {
     .narrow-timeline{
     width:50%;
     margin:auto;
+    }
+    .book-pic{
+      height: 200 px;
+      width: 700px;
     }
 </style>
