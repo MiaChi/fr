7977c580c1fa5c8fee25b2d5a2859ae18f18856d
MiaChi <purple5world@gmail.com>
2018-12-20 16:27:30 +0800

    put referenced style file ahead of App.vue

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import BootstrapVue from 'bootstrap-vue'
-import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import App from './App'
+import router from './router'
+// import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import iView from 'iview'
 
 Vue.use(iView)
 Vue.use(BootstrapVue)
@@ -18,6 +19,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  // store,
   components: { App },
   template: '<App/>'
 })
